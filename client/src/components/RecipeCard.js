fe8c99662ec6c47356f7ca22e07967ccd4b514a4
Andrew Goldman <abg@vt.edu>
2020-01-14 00:18:57 -0500

    dark theme

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -10,10 +10,10 @@ import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -32,10 +32,7 @@ const useStyles = makeStyles(theme => ({
   },
   expandOpen: {
     transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
+  }
 }));
 
 const RecipeCard = props => {
@@ -47,41 +44,50 @@ const RecipeCard = props => {
   };
 
   return (
-    <Card className={classes.card}>
-      <CardHeader
-        title={props.activeRecipe.name}
-      />
-      <CardMedia
-        className={classes.media}
-        image={props.activeRecipe.image}
-      />
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Directions:</Typography>
-          <Typography paragraph>
-            {props.activeRecipe.directions}
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+    <ThemeProvider theme={React.useMemo(
+      () =>
+        createMuiTheme({
+          palette: {
+            type: 'dark'
+          },
+        })
+    )}>
+      <Card inverted className={classes.card}>
+        <CardHeader
+          title={props.activeRecipe.name}
+        />
+        <CardMedia
+          className={classes.media}
+          image={props.activeRecipe.image}
+        />
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Directions:</Typography>
+            <Typography paragraph>
+              {props.activeRecipe.directions}
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </ThemeProvider>
   );
 };
 
