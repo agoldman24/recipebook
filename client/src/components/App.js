b37512a09def833fd864686bc567e50503f88c46
Andrew Goldman <abg@vt.edu>
2020-01-14 21:24:58 -0500

    container test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,20 +9,68 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import RecipeCard from './RecipeCard';
 import SignIn from './SignIn';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-class App extends React.Component {
-  componentDidMount() {
-    this.props.getRandomRecipe();
-  }
+const App = () => {
 
-  render() {
+  //render() {
     const buttonStyle = {
       fontSize:"15px",
       padding:"15px 20px",
       margin:"10px 5px"
     };
     return (
-      <SignIn />
+      <ThemeProvider theme={React.useMemo(
+        () =>
+          createMuiTheme({
+            palette: {
+              type: 'dark'
+            },
+          })
+      )}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <AppBar />
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                style={buttonStyle}
+              >
+                Add User
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                style={buttonStyle}
+                //onClick={this.props.getRandomRecipe}
+              >
+                Get New Recipe
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <RecipeCard/>
+            </Grid>
+          </Grid>
+        </Container>
+      </ThemeProvider>
       // <Box
       //   style={{
       //     backgroundColor:"#282c34",
@@ -71,7 +119,7 @@ class App extends React.Component {
       //   }
       // </Box>
     );
-  }
+  //}
 }
 
 const mapStateToProps = state => {
@@ -82,13 +130,13 @@ const mapStateToProps = state => {
   };
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    getRandomRecipe: () => dispatch({ type: FETCH_RECIPE_REQUESTED })
-  };
-}
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     getRandomRecipe: () => dispatch({ type: FETCH_RECIPE_REQUESTED })
+//   };
+// }
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  {}
 )(App);
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -12,7 +12,6 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 function Copyright() {
   return (
@@ -51,77 +50,68 @@ export default function SignIn() {
   const classes = useStyles();
 
   return (
-    <ThemeProvider theme={React.useMemo(
-      () =>
-        createMuiTheme({
-          palette: {
-            type: 'dark'
-          },
-        })
-    )}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
             </Grid>
-          </form>
-        </div>
-        <Box mt={8}>
-          <Copyright />
-        </Box>
-      </Container>
-    </ThemeProvider>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
