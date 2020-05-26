d603ef583e5f912d2b48bfcf79dd80dd576534a8
Andrew Goldman <abg@vt.edu>
2020-05-25 23:31:35 -0400

    static cancel button

--- client/src/components/RecipeDetail.js
+++ client/src/components/RecipeDetail.js
@@ -5,7 +5,7 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Fab from '@material-ui/core/Fab';
-import CloseIcon from '@material-ui/icons/Close';
+import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
 import IngredientsTable from './IngredientsTable';
 import { isMobile } from 'react-device-detect';
 import { TOGGLE_RECIPE_DETAILS } from '../actions';
@@ -20,6 +20,15 @@ const RecipeDetail = props => {
     width:'30',
     height:'30'
   };
+  const darkBackgroundStyle = {
+    position:'fixed',
+    top:'0',
+    left:'0',
+    height:'100vh',
+    width:'100vw',
+    zIndex:'4',
+    background:'rgba(0,0,0,0.7)'
+  }
   const detailStyle = {
     borderRadius: '0',
     width: isMobile ? '100vw' : '60vw',
@@ -42,51 +51,61 @@ const RecipeDetail = props => {
     lineHeight: '2'
   }
   return (
-    <Card style={detailStyle}>
-      <CardHeader
-        title={
-          <Typography
-            variant="h3"
-            style={{
-              fontWeight:'bold', fontFamily:'Shadows Into Light'
-            }}
-          >
-            {props.name}
-          </Typography>
-        }
-        style={{background:'white', color:'black'}}
-      />
-      <CardMedia
-        style={{height:"0", paddingTop:"100%", position:'relative'}}
-        image={props.image}
-        children={[]}
-      >
-        <div
-          style={{
-            position:'absolute', top:'0', left:'0',
-            width:'100%', height:'30%', verticalAlign:'text-top',
-            backgroundImage:'linear-gradient(white, rgba(0,0,0,0))',
-            color:'black', fontWeight:'bold'
-          }}
-        >
-          <div style={{width:'10%', float:'right'}}>
-            <Fab
-              style={{...fabStyle, float:'right'}}
-              onClick={() => {
-                props.toggleDetailView();
-                document.getElementById('root').style.overflowY = 'auto';
+    <div style={darkBackgroundStyle}>
+      <Card style={detailStyle}>
+        <CardHeader
+          title={
+            <Typography
+              variant="h3"
+              style={{
+                fontWeight:'bold', fontFamily:'Shadows Into Light'
               }}
             >
-              <CloseIcon style={iconStyle}/>
-            </Fab>
+              {props.name}
+            </Typography>
+          }
+          style={{background:'white', color:'black'}}
+        />
+        <CardMedia
+          style={{height:"0", paddingTop:"100%", position:'relative'}}
+          image={props.image}
+          children={[]}
+        >
+          <div
+            style={{
+              position:'absolute', top:'0', left:'0',
+              width:'100%', height:'30%', verticalAlign:'text-top',
+              backgroundImage:'linear-gradient(white, rgba(0,0,0,0))',
+              color:'black', fontWeight:'bold'
+            }}
+          >
+            <div style={{width:'10%', float:'right'}}>
+              <Fab
+                style={{...fabStyle, float:'right'}}
+                onClick={() => {
+                  props.toggleDetailView();
+                  document.getElementById('root').style.overflowY = 'auto';
+                }}
+              >
+                <CloseRoundedIcon
+                  style={{
+                    ...iconStyle,
+                    position:'fixed',
+                    background:'black',
+                    color:'white',
+                    borderRadius:'50px'
+                  }}
+                />
+              </Fab>
+            </div>
           </div>
-        </div>
-        <Typography style={titleStyle} variant="h3">Ingredients</Typography>
-        <IngredientsTable ingredients={props.ingredients}/>
-        <Typography style={titleStyle} variant="h3">Directions</Typography>
-        <Typography style={sectionStyle}>{props.directions}</Typography>
-      </CardMedia>
-    </Card>
+          <Typography style={titleStyle} variant="h3">Ingredients</Typography>
+          <IngredientsTable ingredients={props.ingredients}/>
+          <Typography style={titleStyle} variant="h3">Directions</Typography>
+          <Typography style={sectionStyle}>{props.directions}</Typography>
+        </CardMedia>
+      </Card>
+    </div>
   );
 };
 
