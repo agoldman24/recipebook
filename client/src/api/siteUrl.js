33c74d6e55529964ac6f72cc6bb8665037a7ad5a
Andrew Goldman <abg@vt.edu>
2022-07-12 03:23:43 -0400

    updated api url

--- client/src/api/siteUrl.js
+++ client/src/api/siteUrl.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://guarded-temple-45812.herokuapp.com/api"
+      ? "https://recipebookwebapp.herokuapp.com/api"
       : "http://localhost:9000/api",
 });
